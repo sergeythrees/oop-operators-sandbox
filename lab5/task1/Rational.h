7eca524a17e60e0b0f31cda76a21422408a4b693
Alexey Malov <alexey.malov@cpslabs.net>
2015-04-16 23:21:50 +0300

    Добавлены комментарии к методам

--- lab5/task1/Rational.h
+++ lab5/task1/Rational.h
@@ -1,14 +1,23 @@
 ﻿#pragma once
+
+
+/*
+Класс, моделирующий рациональное число
+*/
 class CRational final
 {
 public:
 	CRational(int numerator = 0, int denominator = 1);
 
+	// Возвращает числитель
 	int GetNumerator() const;
+
+	// Возвращает знаменатель
 	int GetDenominator() const;
 private:
 	int m_numerator;
 	int m_denominator;
 };
 
+// Вычисляет наибольший общий знаменатель чисел a и b
 unsigned GCD(unsigned a, unsigned b);
